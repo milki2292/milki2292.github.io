06f22f18827275d89ef449c4f3cf0e437baff54e
milki2292 <kamil.szmuc2292@gmail.com>
2020-09-02 12:15:02 +0200

    counter

---
+++ Counter/app.js
@@ -0,0 +1,30 @@
+const plusBtn = document.querySelector(".plus");
+const minusBtn = document.querySelector(".minus");
+const counter = document.querySelector(".counter");
+
+plusBtn.addEventListener("click", add);
+minusBtn.addEventListener("click", sub);
+
+counter.innerText = localStorage.getItem("number");
+
+let val=0;
+
+function add() {
+  counter.classList.add("down");
+  val++;
+  counter.addEventListener("transitionend", function () {
+    counter.innerText = val;
+    counter.classList.remove("down");
+    localStorage.setItem("number", val);
+  });
+}
+function sub() {
+    counter.classList.add("down");
+    val--;
+    counter.addEventListener("transitionend", function () {
+      counter.innerText = val;
+      counter.classList.remove("down");
+      localStorage.setItem("number", val);
+    });
+  }
+
